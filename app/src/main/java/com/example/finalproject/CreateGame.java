6b7ed5b80d317516188b74cfbc3bbbefa66d37ce
YYyanyan <yan_yan1231103@163.com>
2023-06-04 10:33:45 +0800

    完成从CreateGame页面到GameCounter页面的数据传输

--- app/src/main/java/com/example/finalproject/CreateGame.java
+++ app/src/main/java/com/example/finalproject/CreateGame.java
@@ -1,14 +1,76 @@
 package com.example.finalproject;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class CreateGame extends AppCompatActivity {
+    EditText date;
+    EditText time;
+    EditText team1;
+    EditText team2;
+    EditText edit_sections;
+    EditText edit_bonus;
+    EditText edit_player_num;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_game);
+        date = findViewById(R.id.game_date);
+        time = findViewById(R.id.game_time);
+        team1 = findViewById(R.id.TeamA_name);
+        team2 = findViewById(R.id.TeamB_name);
+        edit_sections = findViewById(R.id.sections);
+        edit_bonus = findViewById(R.id.bonus);
+        edit_player_num = findViewById(R.id.player_num);
+
+
+    }    
+       
+//        String date = intent.getStringExtra("game_date");
+//        String time = intent.getStringExtra("game_time");
+//        String team1 = intent.getStringExtra("team1");
+//        String team2 = intent.getStringExtra("team2");
+//        Integer sections = intent.getIntExtra("sections",0);
+//        Integer bonus = intent.getIntExtra("bonus",0);
+//        Integer player_num = intent.getIntExtra("player_num",0);
+
+
+    public void create(View save){
+        Log.i(TAG, "creat ");
+        String game_date = date.getText().toString();
+        String game_time = time.getText().toString();
+        String TeamA_name = team1.getText().toString();
+        String TeamB_name = team2.getText().toString();
+        String sections = edit_sections.getText().toString();
+        String bonus = edit_bonus.getText().toString();
+        String player_num = edit_player_num.getText().toString();
+
+        //传输数据
+        Intent GameCounterActivity = new Intent(this,GameCounter.class);
+        //用Bundle放入数据到Intent
+        GameCounterActivity.putExtra("game_date", game_date);
+        GameCounterActivity.putExtra("game_date",game_date);
+        GameCounterActivity.putExtra("game_time",game_time);
+        GameCounterActivity.putExtra("TeamA_name",TeamA_name);
+        GameCounterActivity.putExtra("TeamB_name",TeamB_name);
+        GameCounterActivity.putExtra("sections",Integer.parseInt(sections));
+        GameCounterActivity.putExtra("bonus",Integer.parseInt(bonus));
+        GameCounterActivity.putExtra("player_num",Integer.parseInt(player_num));
+
+        setResult(6,GameCounterActivity);
+        finish();
+
+        startActivity(GameCounterActivity);
+
     }
 }
--- app/src/main/java/com/example/finalproject/GameCounter.java
+++ app/src/main/java/com/example/finalproject/GameCounter.java
@@ -2,13 +2,49 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class GameCounter extends AppCompatActivity {
+    String game_date;
+    String game_time;
+    TextView team1;
+    TextView team2;
+    Integer edit_sections;
+    Integer edit_bonus;
+    Integer edit_player_num;
+    private static final String TAG = "GameCounter";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_counter);
+        team1 = findViewById(R.id.TeamA_name);
+        team2 = findViewById(R.id.TeamB_name);
+        Intent intent = getIntent();
+        game_date = intent.getStringExtra("game_date");
+        game_time = intent.getStringExtra("game_time");
+
+        String TeamA_name = intent.getStringExtra("TeamA_name");
+        team1.setText(TeamA_name);
+        String TeamB_name = intent.getStringExtra("TeamB_name");
+        team2.setText(TeamB_name);
+
+        edit_sections = intent.getIntExtra("sections",0);
+        edit_bonus = intent.getIntExtra("bonus",0);
+        edit_player_num = intent.getIntExtra("player_num",0);
+
+        Log.i(TAG, "onActivityResult: game_date =" + game_date);
+        Log.i(TAG, "onActivityResult: game_time =" + game_time);
+        Log.i(TAG, "onActivityResult: TeamA_name =" + TeamA_name);
+        Log.i(TAG, "onActivityResult: TeamB_name =" + TeamB_name);
+        Log.i(TAG, "onActivityResult: edit_sections =" + edit_sections);
+        Log.i(TAG, "onActivityResult: edit_bonus =" + edit_bonus);
+        Log.i(TAG, "onActivityResult: edit_player_num =" + edit_player_num);
     }
+
+
 }
